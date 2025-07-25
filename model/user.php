9270b4a75df72bbafc21922cee0c8d879aff1cb6
kinoppol <kinoppol@hotmail.com>
2025-07-25 21:11:28 +0700

    Update user.php

--- model/user.php
+++ model/user.php
@@ -6,9 +6,18 @@ class user extends dummy_model{
     }
     function clear_user($data=array()){
         $sql='delete from user_data where user_type_id > 1';
-        //print $sql;
+        print $sql;
+        $result=$this->db->query($sql);
+
+        $sql='select max(id) as max_id from user_data';
         $result=$this->db->query($sql);
         
+        $max_id_data=$result->fetch_assoc();
+
+        $sql='ALTER TABLE user_data AUTO_INCREMENT = '.($max_id_data['max_id']+1);
+        print $sql;
+        $result=$this->db->query($sql);
+
     }
 
 }
